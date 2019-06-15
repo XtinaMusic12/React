f15c15f4f30bafbe4539efc9ced9cd781b83b968
Christina Roufosse <roufosse.christina@gmail.com>
2019-06-15 11:32:04 -0700

    About Us RR & SPA

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
+import About from "./AboutComponent";
 import Contact from "./ContactComponent";
 import DishDetail from "./DishdetailComponent";
 import Menu from "./MenuComponent";
@@ -54,6 +55,10 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route
+            path="/aboutus"
+            render={() => <About leaders={this.state.leaders} />}
+          />
           <Route
             exact
             path="/menu"
