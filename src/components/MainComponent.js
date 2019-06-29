12245a881109903c3e0cf21c611a235d97c13fbe
Christina Roufosse <roufosse.christina@gmail.com>
2019-06-29 12:12:53 -0700

    Task 1 Attempt 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -56,7 +56,11 @@ class Main extends Component {
           }
           promoLoading={this.props.promotions.isLoading}
           promoErrMess={this.props.promotions.errMess}
-          leader={this.props.leaders.filter(leader => leader.featured)[0]}
+          leader={
+            this.props.leaders.leaders.filter(leader => leader.featured)[0]
+          }
+          leaderLoading={this.props.leaders.isLoading}
+          leaderErrMess={this.props.leaders.errMess}
         />
       );
     };
